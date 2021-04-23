450842b5a5d401b0369cc6f1b68b1356f4615917
rajath.warrier <rajath.warrier@vicara.co>
2021-04-23 19:06:06 +0530

    Change order of priority levels in PriorityLevel - Critical to Low

--- lib/model/PriorityLevel.dart
+++ lib/model/PriorityLevel.dart
@@ -3,14 +3,14 @@ import 'package:json_annotation/json_annotation.dart';
 // Enum to control the values for
 // Task Priority.
 enum PriorityLevel {
-  @JsonValue("low")
-  LOW,
-  @JsonValue("medium")
-  MEDIUM,
-  @JsonValue("high")
-  HIGH,
-  @JsonValue("very_high")
-  VERY_HIGH,
   @JsonValue("critical")
   CRITICAL,
+  @JsonValue("very_high")
+  VERY_HIGH,
+  @JsonValue("high")
+  HIGH,
+  @JsonValue("medium")
+  MEDIUM,
+  @JsonValue("low")
+  LOW,
 }
