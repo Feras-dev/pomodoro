b802df30a369ac7e36564fb3e55f273cdd108c76
RajathWarrier <rajath.warrier98@gmail.com>
2021-04-30 10:53:49 +0530

    Format file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,14 +10,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pomodoro/view/addTask.dart';
 
 void main() {
-  Widget createWidgetForTesting({Widget child}){
+  Widget createWidgetForTesting({Widget child}) {
     return MaterialApp(
       home: child,
     );
   }
+
   testWidgets('Pomodoro app test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(createWidgetForTesting(child: AddTaskForm(oldTask: null)));
+    await tester
+        .pumpWidget(createWidgetForTesting(child: AddTaskForm(oldTask: null)));
 
     expect(find.text('No tasks to complete.'), findsNothing);
     // expect(find.text('Press Add Task to add a new one!'), findsOneWidget);
@@ -25,6 +27,5 @@ void main() {
     // Tap the '+' icon and trigger a frame.
     // await tester.tap(find.byIcon(Icons.add));
     // await tester.pump();
-
   });
 }
